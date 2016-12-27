6785c09dcaf6757e6fb9b8d86a2210f9222abc82
Matt Lubel <matt_lubel@hotmail.com>
2016-12-27 22:58:58 +0000

    added more buttons

--- README.md
+++ README.md
@@ -5,8 +5,9 @@ Try it out [here](https://mattlub.github.io/SiNTHESISE)!
 
 ### Planned Improvements
 - [ ] 23/12/16: improve CSS- make it mobile ready (important).
-- [ ] 23/12/16: move start/stop button.
-- [ ] 23/12/16: add more buttons- clear, memory slots, speed settings, maybe (long) chord buttons.
+- [x] 23/12/16: move start/stop button. (DONE 27/12/16)
+- [x] 23/12/16: add more buttons- clear, memory slots (DONE 27/12/16)
+- [ ] 27/12/16: add more buttons- speed settings, maybe (long) chord buttons.
 - [ ] 23/12/16: move audio files out of repo.
 - [ ] 23/12/16: have option to import audio files (or even record on mobile!)
 
--- script.js
+++ script.js
@@ -2,7 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
   var squares = document.querySelectorAll(".square");
   var columns = document.querySelectorAll(".column");
-  var button = document.querySelector(".start-stop-button");
+
+  var startStopButton = document.querySelector(".start-stop-button");
+  var clearButton = document.querySelector(".clear-button");
+  var memoryButton = document.querySelector(".memory-button");
+  var memSlotButtons = document.querySelectorAll(".memory-slot-button");
+  var isInSaveMode = false;
+  var memSlots = [null, null, null];
+
   var audios = document.querySelectorAll("audio");
   var intervalId = null;
   var currCol;
@@ -30,17 +37,64 @@ document.addEventListener("DOMContentLoaded", function () {
   function startStop () {
     if (isPlaying) {
       window.clearInterval(intervalId);
-      button.textContent = "start";
       isPlaying = false;
     }
     else {
       intervalId = window.setInterval(play, 250);
-      button.textContent = "stop";
       isPlaying = true;
     }
   }
 
-  button.addEventListener("click", startStop)
+  function clear () {
+    squares.forEach(s => s.classList.remove("on"));
+  }
+
+  function saveState (slotNumber) {
+    // convert slotNumber to correct index of memSlots array
+    var slotIndex = + slotNumber - 1;
+    memSlots[slotIndex] = Array.from(squares).map(s => s.classList.contains("on"));
+    console.log("saved");
+    console.log(memSlots);
+    memoryButton.classList.remove("save-mode");
+    memSlotButtons[slotIndex].classList.add("full");
+    isInSaveMode = false;
+  }
+
+  function loadState (slotNumber) {
+    // convert slotNumberStr to correct index of memSlots array
+    var slotIndex = + slotNumber - 1;
+    if (memSlots[slotIndex] == null) {
+      console.log("empty slot.");
+      return
+    }
+    else {
+      for (var i=0, n=squares.length; i<n; i++) {
+        if (memSlots[slotIndex][i] === true) {
+          squares[i].classList.add("on");
+        }
+        else {
+          squares[i].classList.remove("on");
+        }
+      }
+    }
+  }
+
+  startStopButton.addEventListener("click", startStop);
+  clearButton.addEventListener("click", clear);
+
+  memoryButton.addEventListener("click", function () {
+    isInSaveMode = !isInSaveMode;
+    this.classList.toggle("save-mode");
+  });
+
+  memSlotButtons.forEach(b => b.addEventListener("click", function (e) {
+    if (isInSaveMode) {
+      saveState(this.dataset["slot"]);
+    }
+    else {
+      loadState(this.dataset["slot"]);
+    }
+  }));
 
   squares.forEach(s => s.addEventListener("click", function () {
     this.classList.toggle("on");
